881a01f4890d38728510317f44ea409382b202f9
April Gao <67171198+gaoad@users.noreply.github.com>
2023-06-11 15:42:59 -0400

    5 of 7 tests passed for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,remove1,remove2) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === remove1 || array[i] === remove2) {
+            array.splice(i,1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
