111034b353f5b22b51163246f9ebac0ad126140c
April Gao <67171198+gaoad@users.noreply.github.com>
2023-06-11 14:22:35 -0400

    Exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+    if (times < 0) return "ERROR";
+    let finalString = "";
+    for (let i = 0; i < times; i++) {
+        finalString += word;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
