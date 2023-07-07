ec25930c0b035440071dad8c27fb57960f55b955
April Gao <67171198+gaoad@users.noreply.github.com>
2023-07-07 17:01:25 -0400

    Pass 04_removeFromArray tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,20 @@
-const removeFromArray = function(array,remove1,remove2) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === remove1 || array[i] === remove2) {
-            array.splice(i,1);
-        }
-    }
-    return array;
-};
+var removeFromArray = function(array, ...args) {
+    return array.filter(val => !args.includes(val));
+}
+
+
+// This one almost worked but wasn't able to pass "removes multiple values" test
+
+// const removeFromArray = function(array, ...remove) {
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j = 0; j < remove.length; j++) {
+//             if (array[i] === remove[j]) {
+//                 array.splice(i,1);
+//             }
+//         }      
+//     }
+//     return array;
+// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
