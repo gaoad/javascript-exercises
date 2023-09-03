721ec6c880b436b7de87e010a4f1591be0b19ec8
April Gao <aprildgao@gmail.com>
2023-09-03 16:23:36 -0400

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+// function round(value, precision) {
+//   var multiplier = Math.pow(10, precision || 0);
+//   return Math.round(value * multiplier) / multiplier;
+// }
+
+const convertToCelsius = function(temp) {
+  return (Math.round(((temp-32) * 5/9) * 10)/10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return (Math.round((temp * 9/5 + 32)*10)/10);
 };
 
 // Do not edit below this line
