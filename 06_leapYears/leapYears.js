61be9c0c98fffbecf91a80fdd8d84d7a3284a103
April Gao <aprildgao@gmail.com>
2023-09-03 15:39:50 -0400

    Complete exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return "ERROR";
+    if (year % 4 == 0) {
+        if (year % 100 !== 0) {
+            return true;
+        }
+        if (year % 100 == 0 && year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 };
+
+//their solution is so much more elegant
 
 // Do not edit below this line
 module.exports = leapYears;
